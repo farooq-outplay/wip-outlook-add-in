5799b66691a1b272cd8c600bd8489f572686fbae
mdfarooqs1111@gmail.com <mdfarooqs1111@gmail.com>
2026-02-18 14:43:55 +0530

    [change] endpoint

--- src/utility/api/sequenceService.ts
+++ src/utility/api/sequenceService.ts
@@ -2,12 +2,12 @@ import { ApiResult } from "../types/apiResultTypes";
 import { mobileApiClient } from "./mobileApiClient";
 
 export interface Sequence {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
 export const getSequences = (): Promise<ApiResult<Sequence[]>> => {
-    return mobileApiClient<ApiResult<Sequence[]>>("/api/v1/cxsequence/search", {
-        method: "GET",
-    });
+  return mobileApiClient<ApiResult<Sequence[]>>("/api/v1/cextsequence/search", {
+    method: "GET",
+  });
 };
